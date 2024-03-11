1293e9c348e3c229647f3e94d2b7f1551769b69c
Lou Onezime <lou.onezime@epitech.eu>
2024-03-11 17:36:24 +0100

    feat: init basic error handling
    with sys/stat

--- src/strace.c
+++ src/strace.c
@@ -7,7 +7,25 @@
 
 #include "strace.h"
 
+#include <stdio.h>
+#include <sys/stat.h>
+
+static int preliminaries(const char *path)
+{
+    struct stat statis;
+
+    if (stat(path, &statis) < OK){
+        fprintf(stderr, "strace: Can't stat '%s`: ", path);
+        perror("");
+        return ERROR;
+    }
+    return OK;
+}
+
 int do_strace(const char *arg)
 {
+    if (preliminaries(arg) == ERROR){
+        return ERROR;
+    }
     return OK;
 }
