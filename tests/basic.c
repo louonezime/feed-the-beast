ee8d76a9b5eb2eb7a49dc8dbaf345dba3cfc9188
Lou Onezime <lou.onezime@epitech.eu>
2024-03-18 21:35:29 +0100

    feat(tests): init base of tests

---
+++ tests/basic.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** feed-the-beast
+** File description:
+** basic
+*/
+
+#include "criterion/criterion.h"
+
+#include "strace.h"
+#include "syscall.h"
+
+Test(basic, binary_process)
+{
+}
